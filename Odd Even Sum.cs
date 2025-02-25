3379fd46933057f705c8029b71b82ae912536205
Inesa Kostova <kostovainesa@gmail.com>
2025-02-25 10:49:03 +0200

    Add files via upload

---
+++ Odd Even Sum.cs
@@ -0,0 +1,27 @@
+﻿int iteration= int.Parse(Console.ReadLine());
+
+int evenSum = 0;
+int oddSum = 0;
+for(int i=0;i<iteration;i++)
+{
+int number = int.Parse(Console.ReadLine());
+    if(i%2==0)
+    {
+        evenSum += number;
+    }
+    else
+    {
+        oddSum += number;
+    }
+
+}
+if(evenSum==oddSum)
+{
+    Console.WriteLine($"Yes");
+    Console.WriteLine($"Sum = {evenSum}");
+}
+else
+{
+    Console.WriteLine($"No");
+    Console.WriteLine($"Diff = {Math.Abs(evenSum-oddSum)}");
+}
